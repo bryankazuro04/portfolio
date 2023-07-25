61e860ddd43661ac6a1057638cfd820173225c22
Bryan <mhdalana211201@gmail.com>
2023-07-25 19:07:47 +0700

    add project

---
+++ src/components/project/project.jsx
@@ -0,0 +1,60 @@
+function Project() {
+  return (
+    <>
+      <section>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae voluptatem, neque omnis suscipit cum eius
+          cupiditate sequi nesciunt vel consectetur! Earum sint explicabo eaque nesciunt aliquid dignissimos! Dicta
+          nostrum at consequuntur iure voluptas earum accusantium perspiciatis ad, aliquam laudantium minima fugiat
+          ducimus quod sapiente quisquam distinctio obcaecati. Fuga illo reprehenderit quia quas doloremque, maxime
+          earum nemo harum amet, ducimus maiores nulla placeat! Dolores iure placeat, vitae aliquam dolorum quam
+          incidunt odio expedita sequi voluptas, eligendi modi culpa vel id sunt natus, suscipit voluptatibus. Nam
+          aliquam facere inventore exercitationem explicabo? Minima consectetur, alias debitis illo dolorum id
+          doloribus? Nam, ea qui.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae voluptatem, neque omnis suscipit cum eius
+          cupiditate sequi nesciunt vel consectetur! Earum sint explicabo eaque nesciunt aliquid dignissimos! Dicta
+          nostrum at consequuntur iure voluptas earum accusantium perspiciatis ad, aliquam laudantium minima fugiat
+          ducimus quod sapiente quisquam distinctio obcaecati. Fuga illo reprehenderit quia quas doloremque, maxime
+          earum nemo harum amet, ducimus maiores nulla placeat! Dolores iure placeat, vitae aliquam dolorum quam
+          incidunt odio expedita sequi voluptas, eligendi modi culpa vel id sunt natus, suscipit voluptatibus. Nam
+          aliquam facere inventore exercitationem explicabo? Minima consectetur, alias debitis illo dolorum id
+          doloribus? Nam, ea qui.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae voluptatem, neque omnis suscipit cum eius
+          cupiditate sequi nesciunt vel consectetur! Earum sint explicabo eaque nesciunt aliquid dignissimos! Dicta
+          nostrum at consequuntur iure voluptas earum accusantium perspiciatis ad, aliquam laudantium minima fugiat
+          ducimus quod sapiente quisquam distinctio obcaecati. Fuga illo reprehenderit quia quas doloremque, maxime
+          earum nemo harum amet, ducimus maiores nulla placeat! Dolores iure placeat, vitae aliquam dolorum quam
+          incidunt odio expedita sequi voluptas, eligendi modi culpa vel id sunt natus, suscipit voluptatibus. Nam
+          aliquam facere inventore exercitationem explicabo? Minima consectetur, alias debitis illo dolorum id
+          doloribus? Nam, ea qui.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae voluptatem, neque omnis suscipit cum eius
+          cupiditate sequi nesciunt vel consectetur! Earum sint explicabo eaque nesciunt aliquid dignissimos! Dicta
+          nostrum at consequuntur iure voluptas earum accusantium perspiciatis ad, aliquam laudantium minima fugiat
+          ducimus quod sapiente quisquam distinctio obcaecati. Fuga illo reprehenderit quia quas doloremque, maxime
+          earum nemo harum amet, ducimus maiores nulla placeat! Dolores iure placeat, vitae aliquam dolorum quam
+          incidunt odio expedita sequi voluptas, eligendi modi culpa vel id sunt natus, suscipit voluptatibus. Nam
+          aliquam facere inventore exercitationem explicabo? Minima consectetur, alias debitis illo dolorum id
+          doloribus? Nam, ea qui.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae voluptatem, neque omnis suscipit cum eius
+          cupiditate sequi nesciunt vel consectetur! Earum sint explicabo eaque nesciunt aliquid dignissimos! Dicta
+          nostrum at consequuntur iure voluptas earum accusantium perspiciatis ad, aliquam laudantium minima fugiat
+          ducimus quod sapiente quisquam distinctio obcaecati. Fuga illo reprehenderit quia quas doloremque, maxime
+          earum nemo harum amet, ducimus maiores nulla placeat! Dolores iure placeat, vitae aliquam dolorum quam
+          incidunt odio expedita sequi voluptas, eligendi modi culpa vel id sunt natus, suscipit voluptatibus. Nam
+          aliquam facere inventore exercitationem explicabo? Minima consectetur, alias debitis illo dolorum id
+          doloribus? Nam, ea qui.
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default Project;
