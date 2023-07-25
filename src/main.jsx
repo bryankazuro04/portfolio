f73232e2df5c7f889376d9102311854f45feb958
Bryan <mhdalana211201@gmail.com>
2023-07-25 22:44:01 +0700

    menambah link page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import "./index.css";
 import Jumbotron from "./components/jumbotron/jumbotron";
 import Project from "./components/project/project";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Education from "./components/education/education";
+import Contact from "./components/contact/contact";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,6 +13,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<Jumbotron />} />
         <Route path="/projects" element={<Project />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
