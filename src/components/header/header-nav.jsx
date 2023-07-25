bc93cf5d4fba338e866f82a1229e8a28058a14be
Bryan <mhdalana211201@gmail.com>
2023-07-25 19:07:22 +0700

    add header nav

---
+++ src/components/header/header-nav.jsx
@@ -0,0 +1,15 @@
+import "./style.css";
+
+function HeaderNav() {
+  return (
+    <>
+      <header>
+        <nav className="container-width">
+          <h1>Bryan Kazuro</h1>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default HeaderNav;
