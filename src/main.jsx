e47be457b5e108f35103bea5ead18ba3feeefcc6
Bryan <mhdalana211201@gmail.com>
2023-07-25 19:08:26 +0700

    masukkan component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import HeaderNav from "./components/header/header-nav.jsx";
+import Jumbotron from "./components/jumbotron/jumbotron";
+import SideNav from "./components/sidebar/sidenav";
+import Project from "./components/project/project";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <HeaderNav />
+    <main className="container-width">
+      <Jumbotron />
+      <Project />
+    </main>
+    <SideNav />
+  </React.StrictMode>
+);
