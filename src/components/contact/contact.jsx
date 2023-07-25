7fd6d38195d3d2eeeb3e72beddfd4280fa6eb4f6
Bryan <mhdalana211201@gmail.com>
2023-07-25 22:43:49 +0700

    menambah page

---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,18 @@
+import HeaderNav from "../header/header-nav"
+import SideNav from "../sidebar/sidenav"
+
+const Contact = () => {
+  return (
+    <>
+      <HeaderNav />
+
+      <main className="container-width">
+
+      </main>
+
+      <SideNav />
+    </>
+  )
+}
+
+export default Contact
---
+++ src/components/education/education.jsx
@@ -0,0 +1,14 @@
+import HeaderNav from "../header/header-nav";
+import SideNav from "../sidebar/sidenav";
+
+const Education = () => {
+  return (
+    <>
+      <HeaderNav />
+      <main className="container-width"></main>
+      <SideNav />
+    </>
+  );
+};
+
+export default Education;
