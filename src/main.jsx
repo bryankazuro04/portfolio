19376a46d4ab3f4c43c38567b871934a8a8e0bcf
Bryan <mhdalana211201@gmail.com>
2023-07-26 22:24:06 +0700

    membuat init AOS

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,10 @@ import Project from "./components/project/project";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Education from "./components/education/education";
 import Contact from "./components/contact/contact";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({ duration: 1000 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
