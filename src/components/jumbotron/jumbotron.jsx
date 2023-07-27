c1d0504f8ea9fa97d1da4391612763f39d31a825
Bryan <mhdalana211201@gmail.com>
2023-07-27 18:14:43 +0700

    menambahkan animasi & class style

--- src/components/jumbotron/jumbotron.jsx
+++ src/components/jumbotron/jumbotron.jsx
@@ -10,11 +10,13 @@ function Jumbotron() {
     <>
       <HeaderNav />
       <main className="container-width flex">
-        <div className="flex flex-center">
-          <section className="flex-2">
-            <h1 className="font-size-2.5">Hi, I'm Muhammad Alana Fauzan</h1>
+        <div className="flex flex-center flex-wrap-reverse flex-wrap-md">
+          <section className="flex-2-md">
+            <h1 className="font-size-2.5" data-aos="fade-down" data-aos-duration="350">
+              Hi, I'm Muhammad Alana Fauzan
+            </h1>
 
-            <p data-aos="zoom-in-right" data-aos-duration="100">
+            <p data-aos="zoom-in" data-aos-duration="100">
               <small
                 style={{
                   color: "rgb(var(--main-color))",
@@ -35,7 +37,7 @@ function Jumbotron() {
               />
             </span>
 
-            <p className="mt-1.25 text-justify" style={{ lineHeight: 1.5 }}>
+            <p className="mt-1.25 text-justify" data-aos="fade-up" data-aos-duration="500" style={{ lineHeight: 1.5 }}>
               Saya seorang mahasiswa informatika dari Universitas Malikussaleh yang berfokus pada pengembangan web.
               Sebagai web developer, saya memiliki keahlian dalam merancang dan mengembangkan berbagai proyek online
               yang menarik. Selain itu, saya juga merupakan alumni dari program MSIB Kampus Merdeka X Dicoding, yang
@@ -45,7 +47,7 @@ function Jumbotron() {
             </p>
 
             <NavLink to="/contact" role="button">
-              <button type="button" className="contact-button mt-1.25">
+              <button type="button" className="contact-button mt-1.25 mb-2" data-aos="fade-up" data-aos-duration="500">
                 Contact Me
               </button>
             </NavLink>
