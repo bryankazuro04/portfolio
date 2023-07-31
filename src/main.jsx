b9095a95e3be304ea06949fa2b885f0a015f8c07
Bryan <mhdalana211201@gmail.com>
2023-07-31 16:22:45 +0700

    revert version & routing

--- src/main.jsx
+++ src/main.jsx
@@ -9,17 +9,17 @@ import Contact from "./components/contact/contact";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-AOS.init({ duration: 1000 });
+AOS.init({ duration: 500 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <switch>
-        <Route index exact path="/" Component={Jumbotron} />
-        <Route exact path="/projects" element={<Project />} />
-        <Route exact path="/education" element={<Education />} />
-        <Route exact path="/contact" element={<Contact />} />
-      </switch>
+      <Routes>
+        <Route index path="/" element={<Jumbotron />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
