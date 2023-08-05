8ad1b5bbad18bf113b336832b866e0c0ceaf4af4
Bryan <mhdalana211201@gmail.com>
2023-08-05 22:17:51 +0700

    add component skills

---
+++ src/components/Skills/SkillItem.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const SkillItem = (props) => {
+  const { title, percentage } = props.skill;
+  return (
+    <>
+      <div className="w-full md:w-1/2">
+        <div className="mb-4 md:mx-4">
+          <div className="flex justify-between items-center mb-1">
+            <h5 className="text-md font-light">{title}</h5>
+            <span className="text-md font-bold" style={{ color: "rgba(var(--main-color), .7)" }}>
+              {percentage}
+            </span>
+          </div>
+          <div className="w-full bg-purple-200 relative h-2 rounded">
+            <div
+              className="absolute h-full left-0 top-0 rounded"
+              style={{ width: `${percentage}`, backgroundColor: "rgb(var(--main-color))" }}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SkillItem;
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import SkillItem from "./SkillItem";
+
+const skillData = [
+  {
+    id: 1,
+    title: "HTML, CSS",
+    percentage: "95%",
+  },
+  {
+    id: 2,
+    title: "Bootstrap 5",
+    percentage: "91%",
+  },
+  {
+    id: 3,
+    title: "TailwindCSS",
+    percentage: "20%",
+  },
+  {
+    id: 4,
+    title: "Javascript",
+    percentage: "67%",
+  },
+  {
+    id: 5,
+    title: "ReactJS",
+    percentage: "5%",
+  },
+  {
+    id: 6,
+    title: "Laravel",
+    percentage: "20%",
+  },
+  {
+    id: 7,
+    title: "Git, Github",
+    percentage: "40%",
+  },
+];
+
+const Skills = () => {
+  return (
+    <>
+      <div className="py-10">
+        <div className="flex flex-wrap">
+          <div className="w-full">
+            <div className="md:mx-4">
+              <h3 className="text-3xl text-center text-[#0ff] font-bold mb-8">Skills</h3>
+            </div>
+          </div>
+          {skillData.map((skill, id) => (
+            <SkillItem skill={skill} key={id} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Skills;
