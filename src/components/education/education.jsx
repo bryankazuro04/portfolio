752a90ea2ec7ab1522e54e2dd9592c886416c0d7
Bryan <mhdalana211201@gmail.com>
2023-08-05 22:18:03 +0700

    add view edu & skills

--- src/components/education/education.jsx
+++ src/components/education/education.jsx
@@ -1,11 +1,17 @@
+import SkillItem from "../Skills/SkillItem";
+import Skills from "../Skills/Skills";
 import HeaderNav from "../header/header-nav";
 import SideNav from "../sidebar/sidenav";
+import VerticalTimeline from "../timeline/timeline";
 
 const Education = () => {
   return (
     <>
       <HeaderNav />
-      <main className="container-width"></main>
+      <main className="container-width">
+        <VerticalTimeline />
+        <Skills />
+      </main>
       <SideNav />
     </>
   );
