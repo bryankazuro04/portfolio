810b8e0a5c802ee82d1e4b2fc4e4beb062e3db71
bryankazuro04 <mhdalana211201@gmail.com>
2023-08-17 14:35:32 +0700

    menambahkan loader

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./loader.css";
 import Home from "./components/home/home";
 import Project from "./components/project/project";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
