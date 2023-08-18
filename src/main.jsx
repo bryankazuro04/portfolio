a6fea1737ce32115d663c75624c0d53ee5822459
Bryan <mhdalana211201@gmail.com>
2023-08-19 01:00:23 +0700

    mengubah route edu to about dan mengubah icon

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import Home from "./components/home/home";
 import Project from "./components/project/project";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Education from "./components/education/education";
+import About from "./components/about/about";
 import Contact from "./components/contact/contact";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Project />} />
-        <Route path="/education" element={<Education />} />
+        <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
