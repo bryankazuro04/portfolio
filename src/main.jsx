ba5b454f1ad430fa3e4245e52083a06c2777f0b8
bryankazuro04 <mhdalana211201@gmail.com>
2025-08-15 15:07:03 +0700

    refactor(main.jsx): replace React.StrictMode with Provider
    Updated the application entry point to use Provider instead of React.StrictMode for state management
    context

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import "./loader.css";
+import "./styles/index.scss";
+import "./styles/loader.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 AOS.init({ duration: 500 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
