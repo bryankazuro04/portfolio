320f31dae5ae5b8f67dad39424d01ddcab7be96f
bryankazuro04 <mhdalana211201@gmail.com>
2025-08-15 18:30:39 +0700

    test(headerbar test unit): add unit tests for HeaderBar component
    Created unit tests for the HeaderBar component to verify its rendering and functionality

--- src/components/Header.test.jsx
+++ src/components/Header.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import HeaderNav from "./header/header-nav";
+import HeaderBar from "./header/HeaderBar";
 
 test("should viewing header navigation", () => {
-  render(<HeaderNav />);
+  render(<HeaderBar />);
   expect(screen.getByText(/Bryan Kazuro/i)).toBeInTheDocument();
 });
